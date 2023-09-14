111fafa4d7e1d476c23753a60f654f703460621a
nazam1998 <nazamfr1998@gmail.com>
2023-09-14 17:29:22 +0500

    feat: change todo item state and added animation on leave

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -6,16 +6,30 @@
       :on-click-handler="onClickHandler"
     />
     <ion-list class="todo-list-wrapper">
-      <ion-item v-for="todo in showCurrentTodo" :key="todo.id">
-        <ion-label>{{ todo.title }}</ion-label>
-      </ion-item>
+      <div v-if="showCurrentTodo.length > 0">
+        <transition-group name="list">
+          <ion-item
+            v-for="todo in showCurrentTodo"
+            :key="todo.id"
+            @click="onTodoItemClick(todo.id)"
+            :color="getTodoColor(todo.isCompleted)"
+          >
+            <ion-label>{{ todo.title }}</ion-label>
+          </ion-item>
+        </transition-group>
+        </div>
+        <div v-else>
+          <ion-item>
+            <ion-label>No Item Found</ion-label>
+          </ion-item>
+        </div>
     </ion-list>
   </div>
 </template>
 
 <script lang="ts">
 import { IonList, IonItem, IonLabel } from "@ionic/vue";
-import { ref } from "vue";
+import { ref, TransitionGroup } from "vue";
 import TodoListHeader from "@/components/TodoListHeader.vue";
 import FormTodo from "./FormTodo.vue";
 
@@ -26,6 +40,7 @@ export default {
     IonLabel,
     TodoListHeader,
     FormTodo,
+    TransitionGroup,
   },
   setup() {
     const todoListHeaders = ref([
@@ -40,30 +55,37 @@ export default {
       { id: 3, title: "Make a project using Ionic Vue ", isCompleted: false },
     ]);
 
-    const getHeaderById = (id: number) => {
-      return todoListHeaders.value.findIndex((item) => item.id === id);
-    };
-    const resetSelection = () => {
-      todoListHeaders.value.forEach((item) => {
-        item.isSelected = false;
-        item;
-      });
-    };
-    const onClickHandler = (id: number) => {
-      const selectedHeaderIndex = getHeaderById(id);
-      const isSelected = todoListHeaders.value[selectedHeaderIndex].isSelected
-      if (!isSelected) {
-        resetSelection();
-        todoListHeaders.value[selectedHeaderIndex].isSelected =
-          !isSelected;
-      }
-    };
-
     return {
       todos,
       todoListHeaders,
-      onClickHandler,
     };
+  },
+  methods: {
+    getHeaderById(id: number) {
+      return this.todoListHeaders.findIndex((item) => item.id === id);
+    },
+    resetSelection() {
+      this.todoListHeaders.forEach((item) => {
+        item.isSelected = false;
+        item;
+      });
+    },
+
+    onClickHandler(id: number) {
+      const selectedHeaderIndex = this.getHeaderById(id);
+      const isSelected = this.todoListHeaders[selectedHeaderIndex]?.isSelected;
+      if (!isSelected) {
+        this.resetSelection();
+        this.todoListHeaders[selectedHeaderIndex].isSelected = !isSelected;
+      }
+    },
+    onTodoItemClick(id: number) {
+      const todoIndex = this.todos.findIndex((todo) => todo.id === id);
+      this.todos[todoIndex].isCompleted = !this.todos[todoIndex].isCompleted;
+    },
+    getTodoColor(isCompleted: boolean) {
+      return isCompleted ? "success" : "";
+    },
   },
   computed: {
     showTodoList() {
@@ -98,5 +120,22 @@ export default {
 .todo-list-wrapper {
   margin: 20px 0;
   border-radius: 8px;
+}
+.list-move, /* apply transition to moving elements */
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.5s ease;
+}
+
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
+
+/* ensure leaving items are taken out of layout flow so that moving
+   animations can be calculated correctly. */
+.list-leave-active {
+  position: absolute;
 }
 </style>
